3be0abce2e58c5959fd4e35ef6d9cc15ab1ee267
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-10-07 12:26:02 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -14,4 +14,15 @@ puts phrase
 
 def say_hello_world_ten_times
   puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
 end
+
+say_hello_world_ten_times
